5a362ab079357f9e44c594685d50a92aeb48490f
kapilsharma <kapil@kapilsharma.info>
2016-08-03 20:32:44 +0530

    Some comments after running again.

--- examples/Phpreboot/performance/basic/quote/quotes.php
+++ examples/Phpreboot/performance/basic/quote/quotes.php
@@ -13,13 +13,15 @@ $stopWatch->addWatches(["single", "double"]);
 
 $name = 'Kapil Sharma';
 
-$stopWatch->start("single");
-$single = 'Name: ' . $name;
-$stopWatch->stop("single");
-
+//Wrong assessment. If we change position of below two blocks, result will change.
+//TODO: Write separate program for single and double quotes string.
 $stopWatch->start("double");
 $double = "Name: $name";
 $stopWatch->stop("double");
+
+$stopWatch->start("single");
+$single = 'Name: ' . $name;
+$stopWatch->stop("single");
 
 printf("Single quotes took %f seconds \n", $stopWatch->getTime("single"));
 printf("Double quotes took %f seconds \n", $stopWatch->getTime("double"));
